b1f09f913116d1f2c5bbd45f354b4709714fb5cc
Woozsh <jordy.pereira@hotmail.com>
2016-08-16 23:24:13 +0200

    finished about, added sponsors, added fac ebook API

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,3 +21,10 @@ $(document).ready(function(){
     $('nav').removeClass('js-open');
   });
 });
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.7";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
