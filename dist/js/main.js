041da94d76555b2071e59fd5a58126427fa1e6e3
Jordy <Jordy>
2016-08-20 23:01:52 +0200

    removed events. form wont submit.

--- dist/js/main.js
+++ dist/js/main.js
@@ -32,3 +32,8 @@ $(document).ready(function(){ //JQUERY
   js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.7";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+//form
+function myFunction() {
+    return false;
+}
